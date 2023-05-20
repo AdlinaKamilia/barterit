71b62456f0a4f52520070fb9dc6302ba5a865d63
AdlinaKamilia <nur_adlina_k@soc.uum.edu.my>
2023-05-21 02:06:38 +0800

    Create tbl_users.sql

---
+++ serverSideScript/tbl_users.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2023 at 08:04 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `barterit_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `user_id` int(6) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_phone` varchar(12) NOT NULL,
+  `user_password` varchar(40) NOT NULL,
+  `user_otp` varchar(6) NOT NULL,
+  `user_datereg` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`user_id`, `user_email`, `user_name`, `user_phone`, `user_password`, `user_otp`, `user_datereg`) VALUES
+(1, 'adlina.azhari@gmail.com', 'Nur Adlina Kamilia Binti Azhari', '01163524626', 'd0dbeaf8b215a4011534b499c7188edf0c07ecf3', '45564', '2023-05-19 11:42:07.111353'),
+(2, 'ain@gmail.com', 'ain', '01159114626', 'f3a249d2e6af8004929a80ebee9c6727769228a9', '82370', '2023-05-19 11:49:27.139842'),
+(3, 'adlina@gmail.com', 'adlina', '01163524626', 'd0dbeaf8b215a4011534b499c7188edf0c07ecf3', '97016', '2023-05-19 11:59:03.532896');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_email` (`user_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `user_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
